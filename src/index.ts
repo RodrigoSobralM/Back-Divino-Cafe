e53016a1584011b5a56ea212c3ad4c0f0d34fb52
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2025-02-15 20:34:26 -0300

    :tada: Feat: Estabelecendo conexão com o banco de dados MySQL.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/connectionDb";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+connectDb();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
